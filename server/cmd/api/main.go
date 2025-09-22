b5e5965b43906508706453d968407a17c3b5389b
Antonio Lentini <111151429+lentscode@users.noreply.github.com>
2025-09-22 21:35:21 +0200

    feat: create separate main scripts and update Docker files

---
+++ server/cmd/api/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/ApexCorse/ephoros/server/internal/api"
+	"github.com/ApexCorse/ephoros/server/internal/db"
+	"github.com/gorilla/mux"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dbUrl := os.Getenv("DB_URL")
+	apiAddress := os.Getenv("API_ADDRESS")
+	if dbUrl == "" {
+		log.Fatalln("[API_MAIN] missing env variables")
+		os.Exit(1)
+	}
+
+	if apiAddress == "" {
+		apiAddress = ":8080"
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	gormDb, err := gorm.Open(postgres.Open(dbUrl))
+	if err != nil {
+		log.Fatalf("[API_MAIN] couldn't open db: %s\n", err.Error())
+		os.Exit(1)
+	}
+	gormDb.AutoMigrate(
+		&db.User{},
+		&db.Section{},
+		&db.Module{},
+		&db.Sensor{},
+		&db.Record{},
+	)
+
+	customDb := db.NewDB(gormDb)
+
+	log.Printf("[API_MAIN] starting API on port %s\n", apiAddress)
+	api := api.NewAPI(&api.APIConfig{
+		DB:      customDb,
+		Router:  mux.NewRouter(),
+		Address: apiAddress,
+	})
+	go api.Start()
+	log.Println("[API_MAIN] started server")
+
+	<-ctx.Done()
+}
