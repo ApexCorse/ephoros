408c0daad263860691e8302d8afc8dfcf8e18e12
Antonio Lentini <111151429+lentscode@users.noreply.github.com>
2025-10-08 11:10:42 +0200

    test: add another test case

--- embedded/test/configuration_test.c
+++ embedded/test/configuration_test.c
@@ -2,6 +2,7 @@
 #include <unity.h>
 
 #include "json/example_valid.h"
+#include "json/example_valid_2.h"
 #include "json/example_invalid_1.h"
 #include "json/example_invalid_2.h"
 #include "json/example_invalid_3.h"
@@ -16,8 +17,27 @@ void test_configuration_initialize() {
 
 	TEST_ASSERT_NOT_NULL(cfg);
 	TEST_ASSERT_EQUAL(CONFIGURATION_err_ok, err);
+	TEST_ASSERT_EQUAL(1, cfg->n);
 	TEST_ASSERT_EQUAL_STRING("NTC1", cfg->configs[0].id);
 	TEST_ASSERT_EQUAL_STRING("Battery/Module-1/NTC-1", cfg->configs[0].topic);
+	CONFIGURATION_cleanup(cfg);
+}
+
+void test_configuration_initialize_2() {
+	CONFIGURATION_configs* cfg = NULL;
+	CONFIGURATION_err err = CONFIGURATION_initialize(
+		&cfg,
+		example_valid_2_json,
+		example_valid_2_json_len
+	);
+
+	TEST_ASSERT_NOT_NULL(cfg);
+	TEST_ASSERT_EQUAL(CONFIGURATION_err_ok, err);
+	TEST_ASSERT_EQUAL(2, cfg->n);
+	TEST_ASSERT_EQUAL_STRING("NTC1", cfg->configs[0].id);
+	TEST_ASSERT_EQUAL_STRING("Battery/Module-1/NTC-1", cfg->configs[0].topic);
+	TEST_ASSERT_EQUAL_STRING("NTC2", cfg->configs[1].id);
+	TEST_ASSERT_EQUAL_STRING("Battery/Module-2/NTC-2", cfg->configs[1].topic);
 	CONFIGURATION_cleanup(cfg);
 }
 
@@ -92,6 +112,7 @@ void app_main(void) {
   UNITY_BEGIN();
 
   RUN_TEST(test_configuration_initialize);
+  RUN_TEST(test_configuration_initialize_2);
   RUN_TEST(test_configuration_initialize_invalid_1_config);
   RUN_TEST(test_configuration_initialize_invalid_2_config);
   RUN_TEST(test_configuration_initialize_invalid_3_config);
